372421f864abdca9ae1ba124645d43980982c31e
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2025-06-24 11:07:37 -0300

    Creacion DTO y del Servicio e Interfaz (sin logica)

---
+++ Servicios/LogicaNegocio/Empleado/EmpleadoServicio.cs
@@ -0,0 +1,32 @@
+﻿using Servicios.LogicaNegocio.Empleado.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.LogicaNegocio.Empleado
+{
+    public class EmpleadoServicio : IEmpleadoServicio
+    {
+        public void Eliminar(long empleadoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Insertar(EmpleadoDTO empleadoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modificar(EmpleadoDTO empleadoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmpleadoDTO> ObtenerEmpleados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Servicios/LogicaNegocio/Empleado/IEmpleadoServicio.cs
@@ -0,0 +1,20 @@
+﻿using Servicios.LogicaNegocio.Empleado.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.LogicaNegocio.Empleado
+{
+    public interface IEmpleadoServicio
+    {
+        IEnumerable<EmpleadoDTO> ObtenerEmpleados();
+
+        long Insertar(EmpleadoDTO empleadoDto);
+
+        void Modificar(EmpleadoDTO empleadoDto);
+
+        void Eliminar(long empleadoId);
+    }
+}
