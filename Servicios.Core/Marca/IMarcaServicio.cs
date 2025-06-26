476caadf899f64b610e781f81ce1678d870ad02f
MartinMiguelRR <martin97.rodri@gmail.com>
2025-06-25 22:05:22 -0300

    mod ubicacion carpetas marca y categoria, add archivos marca dto, interfaz y servicios

---
+++ Servicios.Core/Marca/IMarcaServicio.cs
@@ -0,0 +1,24 @@
+﻿using Servicios.Core.Marca.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Core.Marca
+{
+    public interface IMarcaServicio
+    {
+        IEnumerable<MarcaDTO> ObtenerMarca(string cadenaBuscar);
+
+        //IEnumerable<MarcaDTO> ObtenerMarcaEliminada(string cadenaBuscar);
+
+        MarcaDTO ObtenerPorId(long marca);
+
+        long Insertar(MarcaDTO marcaDTO);
+
+        void Modificar(MarcaDTO marcaDTO);
+
+        void Eliminar(long marcaId);
+    }
+}
---
+++ Servicios.Core/Marca/MarcaServicio.cs
@@ -0,0 +1,38 @@
+﻿using AccesoDatos;
+using Servicios.Core.Marca.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Core.Marca
+{
+    public class MarcaServicio : IMarcaServicio
+    {
+        public void Eliminar(long marcaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Insertar(MarcaDTO marcaDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modificar(MarcaDTO marcaDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MarcaDTO> ObtenerMarca(string cadenaBuscar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MarcaDTO ObtenerPorId(long marca)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
