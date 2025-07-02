7a43f6784b6b05db1680fac391aca3e1d07d434c
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2025-07-02 10:37:36 -0300

    Crecion de los serivicos y dto correspondiente. Detalle: en las propiedades cambiar los long a string

---
+++ Servicios/LogicaNegocio/Empleado/Rol/RolServicio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.LogicaNegocio.Empleado.Rol
+{
+    public class RolServicio : IRolServicio
+    {
+    }
+}
