a3bdbedeba8a239469df8502aadaa81c7a87cb28
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2025-07-03 17:48:34 -0300

    Creacion clase UsuarioLogeado

---
+++ Presentacion.AccesoAlSistema/UsuarioLogeado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.AccesoAlSistema
+{
+    public class UsuarioLogeado
+    {
+        public long PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Username { get; set; }
+    }
+}
