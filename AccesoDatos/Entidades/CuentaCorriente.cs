885cd8239c9588238da4821d9b4fad1d6aeddcba
MartinMiguelRR <martin97.rodri@gmail.com>
2025-09-05 03:32:21 -0300

    add entidades para migracion

---
+++ AccesoDatos/Entidades/CuentaCorriente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Entidades
+{
+    public class CuentaCorriente
+    {
+        [Key]
+        public long CuentaCorrienteId { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal LimiteCredito { get; set; }
+        public bool EstaEliminado { get; set; }
+        public ICollection<Cliente> Clientes { get; set; }
+
+        //public ICollection<MovimientoCuentaCorriente> MovimientosCuentaCorriente { get; set; } // Relación uno a muchos con MovimientosCuentaCorriente
+    }
+}
