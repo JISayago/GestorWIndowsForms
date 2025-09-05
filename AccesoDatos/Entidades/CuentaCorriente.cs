53a77c23955720361564b5a7fd617d35fcfcda44
MartinMiguelRR <martin97.rodri@gmail.com>
2025-09-05 03:09:48 -0300

    add key cuentaCorriente

--- AccesoDatos/Entidades/CuentaCorriente.cs
+++ AccesoDatos/Entidades/CuentaCorriente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace AccesoDatos.Entidades
 {
     public class CuentaCorriente
     {
+        [Key]
         public long CuentaCorrienteId { get; set; }
         public decimal Saldo { get; set; }
         public decimal LimiteCredito { get; set; }
