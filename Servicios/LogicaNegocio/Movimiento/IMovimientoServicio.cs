808ddb959ae9b7fd2ccf7a7b7af3043d0603dcc1
MartinMiguelRR <martin97.rodri@gmail.com>
2025-11-12 02:52:36 -0300

    movimiento service y dto

---
+++ Servicios/LogicaNegocio/Movimiento/IMovimientoServicio.cs
@@ -0,0 +1,16 @@
+﻿using Servicios.LogicaNegocio.Movimiento.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.LogicaNegocio.Movimiento
+{
+    public interface IMovimientoServicio
+    {
+        void CrearMovimientoVenta(Venta.DTO.VentaDTO ventaDto);
+        MovimientoDTO ObtenerMovimientoPorId(long movimientoId);
+        IEnumerable<MovimientoDTO> ObtenerMovimientos();
+    }
+}
--- Servicios/LogicaNegocio/Movimiento/MovimientoService.cs
+++ Servicios/LogicaNegocio/Movimiento/MovimientoService.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccesoDatos;
+using Servicios.LogicaNegocio.Movimiento.DTO;
+using Servicios.LogicaNegocio.Venta.DTO;
+
 
 namespace Servicios.LogicaNegocio.Movimiento
 {
@@ -14,5 +18,59 @@ namespace Servicios.LogicaNegocio.Movimiento
          * obtenerMovimientoPorId
          * obtenerMovimientos
         */
+
+        public void CrearMovimientoVenta(VentaDTO ventaDto) 
+        {
+            // Lógica para crear un movimiento basado en la venta proporcionada
+            var context = new GestorContextDBFactory().CreateDbContext(null);
+            var movimiento = new AccesoDatos.Entidades.Movimiento
+            {
+                NumeroMovimiento = "MOV" + ventaDto.NumeroVenta + "VENTA", // Ejemplo de generación de número de movimiento
+                IdVenta = ventaDto.VentaId,
+                TipoMovimiento = 1, // Ingreso
+                Monto = ventaDto.Total,
+                FechaMovimiento = DateTime.Now,
+                EstaEliminado = false
+            };
+
+            context.Movimientos.Add(movimiento);
+            context.SaveChanges();
+        }
+
+        public MovimientoDTO ObtenerMovimientoPorId(long movimientoId)
+        {
+            // Lógica para obtener un movimiento por su ID
+            var context = new GestorContextDBFactory().CreateDbContext(null);
+            var movimiento = context.Movimientos.FirstOrDefault(m => m.MovimientoId == movimientoId && !m.EstaEliminado);
+            // Retornar o procesar el movimiento según sea necesario
+            return new MovimientoDTO
+            {
+                MovimientoId = movimiento.MovimientoId,
+                NumeroMovimiento = movimiento.NumeroMovimiento,
+                IdVenta = movimiento.IdVenta,
+                TipoMovimiento = movimiento.TipoMovimiento,
+                Monto = movimiento.Monto,
+                FechaMovimiento = movimiento.FechaMovimiento,
+                EstaEliminado = movimiento.EstaEliminado
+            };
+        }
+
+        public IEnumerable<MovimientoDTO> ObtenerMovimientos()
+        {
+            // Lógica para obtener todos los movimientos
+            var context = new GestorContextDBFactory().CreateDbContext(null);
+            var movimientos = context.Movimientos.Where(m => !m.EstaEliminado).ToList();
+            // Retornar o procesar la lista de movimientos según sea necesario
+            return movimientos.Select(m => new MovimientoDTO
+            {
+                MovimientoId = m.MovimientoId,
+                NumeroMovimiento = m.NumeroMovimiento,
+                IdVenta = m.IdVenta,
+                TipoMovimiento = m.TipoMovimiento,
+                Monto = m.Monto,
+                FechaMovimiento = m.FechaMovimiento,
+                EstaEliminado = m.EstaEliminado
+            });
+        }
     }
 }
