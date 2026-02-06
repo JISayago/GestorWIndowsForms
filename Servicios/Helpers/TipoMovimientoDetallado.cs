98e530225f0b4bff9f1766f7b38c31bba960d17a
MartinMiguelRR <martin97.rodri@gmail.com>
2026-02-06 20:31:23 -0300

    add helper tipoMovimientoDetallado

---
+++ Servicios/Helpers/TipoMovimientoDetallado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Helpers
+{
+    public enum TipoMovimientoDetallado
+    {
+        Cancelacion = 1,
+        CuentaCorriente = 2,
+        Stock = 3,
+        Venta = 4,
+        Compra = 5
+    }
+}
