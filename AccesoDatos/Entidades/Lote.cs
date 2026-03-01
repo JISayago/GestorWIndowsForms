1a7c8a562d12303baab24732fb9a90be3e1e72f7
MartinMiguelRR <martin97.rodri@gmail.com>
2026-03-01 18:38:05 -0300

    fix nombr entidad lote, loteDto y funcion alta

--- AccesoDatos/Entidades/Lote.cs
+++ AccesoDatos/Entidades/Lote.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace AccesoDatos.Entidades
 {
-    public class Lotes
+    public class Lote
     {
         public long LoteId { get; set; }
         public long ProductoId { get; set; }
+        public int Cantidad { get; set; }
         public string NumeroLote { get; set; } //autogenerado por el sistema
         public string NombreLote { get; set; } //
         public string Descripcion { get; set; }
-        public DateTime fechaAlta { get; set; }
-        public DateTime? fechaVencimiento { get; set; }
-        public bool EstaEliminado { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime? FechaVencimiento { get; set; }
+        public bool EstaEliminado { get; set; } //no hace falta en el dto, solo se modifica cuando ya se creó el lote
 
         public Producto Producto { get; set; }
         public List<Movimiento> Movimientos { get; set; }
---
+++ Servicios/LogicaNegocio/Producto/DTO/LoteDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.LogicaNegocio.Producto.DTO
+{
+    public class LoteDTO
+    {
+        public int Id { get; set; }
+        public long IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public string NumeroLote { get; set; }
+        public string NombreLote { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime? FechaVencimiento { get; set; }
+    }
+}
