66a6d3b80d71e94128667a329c26e3648edfa069
MartinMiguelRR <martin97.rodri@gmail.com>
2026-03-05 17:18:20 -0300

    migracion producto,mod entidad lote

--- AccesoDatos/Entidades/Lote.cs
+++ AccesoDatos/Entidades/Lote.cs
@@ -10,13 +10,15 @@ namespace AccesoDatos.Entidades
     {
         public long LoteId { get; set; }
         public long IdProducto { get; set; }
-        public int Cantidad { get; set; }
+        public int StockIncial { get; set; }
+        public int StockActual { get; set; }
         public string NumeroLote { get; set; } //autogenerado por el sistema
         public string NombreLote { get; set; } //
         public string Descripcion { get; set; }
         public DateTime FechaAlta { get; set; }
         public DateTime? FechaVencimiento { get; set; }
-        public bool EstaEliminado { get; set; } //no hace falta en el dto, solo se modifica cuando ya se creó el lote
+        public bool EstaVencido { get; set; } //se calcula en base a la fecha de vencimiento, no se guarda en la base de datos
+        public bool EstaActivo { get; set; } //se calcula en base al stock actual, no se guarda en la base de datos, creo que deberia ya que hay casos en los que no estara activo y tendra stock disponible
 
         public Producto Producto { get; set; }
         public List<Movimiento> Movimientos { get; set; }
