a505d377b6af78ad67b58a12cc1d8750fddc42d5
MartinMiguelRR <martin97.rodri@gmail.com>
2026-03-24 19:10:30 -0300

    Migracion add propiedad TieneVencimiento a Producto

---
+++ AccesoDatos/Migrations/20260324220524_AlterProductoTieneVencimiento.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterProductoTieneVencimiento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "tiene_vencimiento",
+                table: "Productos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tiene_vencimiento",
+                table: "Productos");
+        }
+    }
+}
