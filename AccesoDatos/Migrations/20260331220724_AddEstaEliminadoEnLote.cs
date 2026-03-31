5dc22a04059737e1b1c568644d6071e472d68d18
MartinMiguelRR <martin97.rodri@gmail.com>
2026-03-31 19:09:09 -0300

    Migracion add EstaEliminado en Lotes

---
+++ AccesoDatos/Migrations/20260331220724_AddEstaEliminadoEnLote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstaEliminadoEnLote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "esta_eliminado",
+                table: "Lotes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "esta_eliminado",
+                table: "Lotes");
+        }
+    }
+}
