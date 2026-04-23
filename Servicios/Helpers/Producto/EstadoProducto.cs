d9f6d995917f1a47cd91d198e195c230b20de243
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2026-04-23 10:42:49 -0300

    creacion enum Estado Producto

---
+++ Servicios/Helpers/Producto/EstadoProducto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Helpers.Producto
+{
+    public enum EstadoProducto
+    {
+        Normal = 0,
+        Vencido = 1,
+        Discontunuado = 2,
+        SinStock = 3
+    }
+}
