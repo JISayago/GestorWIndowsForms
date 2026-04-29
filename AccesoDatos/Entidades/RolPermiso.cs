a4fbbea37ddb672ccd60c2dd751d249c3c0f0df6
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2026-04-29 16:57:08 -0300

    clases necesarios

---
+++ AccesoDatos/Entidades/RolPermiso.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Entidades
+{
+    public class RolPermiso
+    {
+        [Key]
+        public long RolPermisoId { get; set; }
+        public long IdRol { get; set; }
+        public long IdPermiso { get; set; }
+
+        public Rol Rol { get; set; }
+        public Permiso Permiso { get; set; }    
+
+    }
+}
