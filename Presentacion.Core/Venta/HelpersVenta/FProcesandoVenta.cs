c8288092dd7f17b02e276c5e58f61363bd70dc8f
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2026-06-02 17:44:26 -0300

    cambio previo a la cagada q me voy a mandar

---
+++ Presentacion.Core/Venta/HelpersVenta/FProcesandoVenta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Presentacion.Formularios
+{
+    public partial class FProcesando : Form
+    {
+        private Label lblEstado;
+        private ProgressBar progressBar;
+
+        public FProcesando()
+        {
+            InicializarComponentes();
+        }
+
+        private void InicializarComponentes()
+        {
+            Text = "Procesando";
+            StartPosition = FormStartPosition.CenterParent;
+
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            ControlBox = false;
+
+            MaximizeBox = false;
+            MinimizeBox = false;
+
+            Width = 400;
+            Height = 140;
+
+            lblEstado = new Label
+            {
+                Text = "Procesando...",
+                AutoSize = false,
+                Width = 340,
+                Height = 25,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Location = new Point(20, 15)
+            };
+
+            progressBar = new ProgressBar
+            {
+                Width = 340,
+                Height = 25,
+                Location = new Point(20, 55),
+                Style = ProgressBarStyle.Marquee
+            };
+
+            Controls.Add(lblEstado);
+            Controls.Add(progressBar);
+        }
+
+        public void ActualizarEstado(string texto)
+        {
+            lblEstado.Text = texto;
+            lblEstado.Refresh();
+            Application.DoEvents();
+        }
+    }
+}
