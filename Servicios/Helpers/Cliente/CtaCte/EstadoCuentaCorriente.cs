53fedbc5c682603741672a1532ce1ea82f1ce516
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2026-07-27 11:46:30 -0300

    se agrega dcetalle de estado enum de estado de cta cte

--- Servicios/Helpers/Cliente/CtaCte/EstadoCuentaCorriente.cs
+++ Servicios/Helpers/Cliente/CtaCte/EstadoCuentaCorriente.cs
@@ -2,8 +2,9 @@
 {
     public enum EstadoCuentaCorriente
     {
-        Activa = 1,
-        Suspendida = 2, //Vencida 
-        Cerrada = 3
+        Inactiva = 0,// creada sin activarse nunca estado Default
+        Activa = 1, // avivada
+        Suspendida = 2, //Vencida por fecha o por deuda 
+        Cerrada = 3 // para su no uso. ver el alcance
     }
 }
