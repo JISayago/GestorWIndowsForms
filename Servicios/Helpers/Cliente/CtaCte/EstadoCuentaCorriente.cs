cc781b7088ea7d403832f84893f68bec225e8c1e
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2026-07-28 17:08:22 -0300

    se saca estado de cta cte q no se va a usar

--- Servicios/Helpers/Cliente/CtaCte/EstadoCuentaCorriente.cs
+++ Servicios/Helpers/Cliente/CtaCte/EstadoCuentaCorriente.cs
@@ -2,7 +2,6 @@
 {
     public enum EstadoCuentaCorriente
     {
-        Inactiva = 0,// creada sin activarse nunca estado Default
         Activa = 1, // avivada
         Suspendida = 2, //Vencida por fecha o por deuda 
         Cerrada = 3 // para su no uso. ver el alcance
